38602a882affb90fd3177d676f3328077535892c
Robbemans <robertblaak@outlook.com>
2025-12-01 16:25:39 +0100

    add movement

--- main.py
+++ main.py
@@ -22,10 +22,11 @@ def main():
             
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         clock.tick(60)
         dt += (clock.tick(60)/1000)
-
+        
 
 
 if __name__ == "__main__":
