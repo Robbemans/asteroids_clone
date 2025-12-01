6485574123e22dfbf6360f5d413449437d618661
Robbemans <robertblaak@outlook.com>
2025-12-01 10:46:44 +0100

    add fps limit

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     
     while True:
         log_state()
@@ -13,6 +16,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt += (clock.tick(60)/1000)
+        print(dt)
 
 
 
